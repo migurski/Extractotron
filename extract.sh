f9b5104a4f1f492425546b092e5944cf70da4a7f
Michal Migurski <mike@stamen.com>
2011-09-18 21:32:06 -0700

    Improved logging

--- extract.sh
+++ extract.sh
@@ -4,13 +4,19 @@
 # #!/bin/sh
 # K=<your AWS access key>
 # S=<your AWS access secret>
-# curl -s https://raw.github.com/migurski/Extractotron/master/extract.sh | KEY=$K SECRET=$S sh > progress.txt 2>&1
+# B=<your bucket name>
+# U=https://raw.github.com/migurski/Extractotron/master/extract.sh
+# curl -s $U | KEY=$K SECRET=$S BUCKET=$B sh > /mnt/progress.txt 2>&1
 #
+
+echo '# begin', `date` > log.txt
+echo 'Installing, see install.txt'
 
 apt-get install -y \
     apache2-mpm-worker \
     openjdk-6-jre-headless \
-    python-boto
+    python-boto \
+  > install.txt 2>&1
 
 rm -rfv /var/www
 ln -sfv /mnt /var/www
@@ -27,6 +33,7 @@ curl -OL "http://download.geofabrik.de/osm/north-america/us/connecticut.osm.bz2"
 
 echo '------------------------------------------------------------------------------'
 
+echo '# extract', `date` >> log.txt
 mkdir ex
 
 bunzip2 -c connecticut.osm.bz2 | osmosis-*/bin/osmosis --rx file=- --log-progress interval=60 \
@@ -40,20 +47,29 @@ echo '--------------------------------------------------------------------------
 
 python <<CODE
 
+from os import stat
 from glob import glob
 from sys import stderr
 from os.path import basename
+
 from boto.s3.connection import S3Connection
 from boto.s3.bucket import Bucket
 
 types = dict(bz2='application/x-bzip2', pbf='application/octet-stream')
-bucket = Bucket(S3Connection('$KEY', '$SECRET'), 'osm-metro-extracts')
+bucket = Bucket(S3Connection('$KEY', '$SECRET'), '$BUCKET')
+log = open('log.txt', 'a')
 
 for file in glob('ex/*.osm.???'):
     print >> stderr, file
+    print >> log, name, stat(file).st_size
+
     name = basename(file)
     type = types[name[-3:]]
     key = bucket.new_key(name)
     key.set_contents_from_file(open(file), policy='public-read', headers={'Content-Type': type})
+
+log.close()
+key = bucket.new_key('log.txt')
+key.set_contents_from_file(open('log.txt'), policy='public-read', headers={'Content-Type': 'text/plain'})
 
 CODE
