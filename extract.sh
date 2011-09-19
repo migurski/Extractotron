4837518c358aeca5a760bc32b0f7296350c05a23
Michal Migurski <mike@stamen.com>
2011-09-18 22:48:10 -0700

    Added more cities and expanded to full planet

--- extract.sh
+++ extract.sh
@@ -29,21 +29,31 @@ tar -xzf osmosis-latest.tgz
 echo '------------------------------------------------------------------------------'
 
 echo 'Downloading, see download.txt'
-curl -OL "http://download.geofabrik.de/osm/north-america/us/california.osm.bz2" > download.txt 2>&1
+curl -OL "http://planet.openstreetmap.org/planet-latest.osm.bz2" > download.txt 2>&1
 
 echo '------------------------------------------------------------------------------'
 
 echo '# extract', `date` >> log.txt
 mkdir ex
 
-bunzip2 -c california.osm.bz2 | osmosis-*/bin/osmosis --rx file=- \
+bunzip2 -c planet-latest.osm.bz2 | osmosis-*/bin/osmosis --rx file=- \
     --log-progress interval=60 \
-    --tee outputCount=2 \
+    --tee outputCount=7 \
     \
-    --bb top=37.9203 left=-122.8244 bottom=37.5489 right=-121.7752 \
+    --bb top=37.955 left=-122.737 bottom=37.449 right=-122.011 \
         --tee outputCount=2 --wx file=ex/san-francisco.osm.bz2 --wb file=ex/san-francisco.osm.pbf \
-    --bb top=34.14477 left=-118.5438 bottom=33.9262 right=-118.1346 \
-        --tee outputCount=2 --wx file=ex/los-angeles.osm.bz2 --wb file=ex/los-angeles.osm.pbf
+    --bb top=38.719 left=-123.640 bottom=36.791 right=-121.025 \
+        --tee outputCount=2 --wx file=ex/sf-bay-area.osm.bz2 --wb file=ex/sf-bay-area.osm.pbf \
+    --bb top=34.583 left=-119.437 bottom=33.298 right=-116.724 \
+        --tee outputCount=2 --wx file=ex/los-angeles.osm.bz2 --wb file=ex/los-angeles.osm.pbf \
+    --bb top=41.097 left=-74.501 bottom=40.345 right=-73.226 \
+        --tee outputCount=2 --wx file=ex/new-york.osm.bz2 --wb file=ex/new-york.osm.pbf \
+    --bb top=51.984 left=-1.115 bottom=50.941 right=0.895 \
+        --tee outputCount=2 --wx file=ex/london.osm.bz2 --wb file=ex/london.osm.pbf \
+    --bb top=52.994 left=12.260 bottom=51.849 right=14.699 \
+        --tee outputCount=2 --wx file=ex/berlin.osm.bz2 --wb file=ex/berlin.osm.pbf \
+    --bb top=56.200 left=36.870 bottom=55.285 right=38.430 \
+        --tee outputCount=2 --wx file=ex/moscow.osm.bz2 --wb file=ex/moscow.osm.pbf
 
 echo '------------------------------------------------------------------------------'
 
