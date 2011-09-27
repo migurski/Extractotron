e8c0fe233af83f7fba059d1eaa57e8eff69e96c2
Michal Migurski <mike@stamen.com>
2011-09-26 18:05:20 -0700

    Made possible to generate user-data without running an actual instance

--- run-instance.py
+++ run-instance.py
@@ -5,7 +5,7 @@ from boto.ec2 import EC2Connection
 
 parser = OptionParser(usage="%prog [options] <aws key> <aws secret> <s3 bucket>")
 
-defaults = dict(ami_id='ami-68ad5201', type='m1.large')
+defaults = dict(ami_id='ami-68ad5201', type='m1.large', run=True)
 
 parser.set_defaults(**defaults)
 
@@ -14,6 +14,9 @@ parser.add_option('--ami-id', dest='ami_id',
 
 parser.add_option('--type', dest='type',
                   help='Instance type, default %(type)s' % defaults)
+
+parser.add_option('--no-run', dest='run', action='store_false',
+                  help="Don't actually run the instance, just output user-data.")
 
 if __name__ == '__main__':
     
@@ -29,5 +32,9 @@ if __name__ == '__main__':
     user_data = user_data.replace('$SECRET', aws_secret)
     user_data = user_data.replace('$BUCKET', s3_bucket)
     
-    conn = EC2Connection(aws_key, aws_secret)
-    print conn.run_instances(options.ami_id, instance_type=options.type, user_data=user_data)
+    if options.run:
+        conn = EC2Connection(aws_key, aws_secret)
+        print conn.run_instances(options.ami_id, instance_type=options.type, user_data=user_data)
+    
+    else:
+        print user_data
