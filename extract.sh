fb402b8971ca37fe6d4faef6f48b98493738b19c
Michal Migurski <mike@stamen.com>
2011-09-26 18:06:01 -0700

    Added coastline error checker run

--- extract.sh
+++ extract.sh
@@ -1,17 +1,33 @@
-#!/bin/sh
+#!/bin/sh -ex
+
+# Make it possible to watch on port 80
+rm -rfv /var/www
+ln -sfv /mnt /var/www
+
+# Osmosis uses a lot of /tmp so put it on the EBS volume
+mv -v /tmp /mnt/
+ln -sv /mnt/tmp /tmp
+
 
 cd /mnt
 
 echo '# begin', `date` > log.txt
 
-apt-get install -y \
-    apache2-mpm-worker \
-    openjdk-6-jre-headless \
-    python-boto \
-  > install.txt 2>&1
+apt-get update > install.txt 2>&1
 
-rm -rfv /var/www
-ln -sfv /mnt /var/www
+apt-get install -y \
+    apache2-mpm-worker openjdk-6-jre-headless python-boto libshp-dev libxml2-dev \
+    libproj-dev zlib1g-dev libbz2-dev mapnik-utils gdal-bin subversion make zip \
+ >> install.txt 2>&1
+
+PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Tree::R' >> install.txt 2>&1
+PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Bit::Vector' >> install.txt 2>&1
+
+svn co http://svn.openstreetmap.org/applications/utils/coastcheck cc >> install.txt 2>&1
+cd cc
+make >> install.txt 2>&1
+cd ..
+
 
 curl -sOL http://dev.openstreetmap.org/~bretth/osmosis-build/osmosis-latest.tgz
 tar -xzf osmosis-latest.tgz
@@ -21,17 +37,35 @@ curl -OL "http://planet.openstreetmap.org/planet-latest.osm.bz2" > download.txt 
 echo '# extract', `date` >> log.txt
 mkdir ex
 
-osmosis.sh;
+osmosis.sh
 
-python <<CODE
+
+mkdir coast
+
+cc/osm2coast coastline.osm.bz2 | gzip > coast/coastline.osm.gz
+cc/merge-coastlines.pl coast/coastline.osm.gz > coast/coast-merged.txt
+cc/coast2shp coast/coast-merged.txt coast/coastline.osm.gz coast/coastline > /dev/null
+cc/closeshp coast/coastline_c coast/coastline_i coast/processed > /dev/null
+shapeindex coast/coastline_c coast/coastline_i coast/coastline_p coast/processed_p coast/processed_i
+
+mkdir ex/merc
+mkdir ex/wgs84
+
+for NAME in processed_p processed_i coastline_i; do
+    ogr2ogr -a_srs "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over" ex/merc/$NAME.shp coast/$NAME.shp
+    tar -C ex/merc -cvf - ex/merc/$NAME.dbf ex/merc/$NAME.prj ex/merc/$NAME.shp ex/merc/$NAME.shx | bzip2 > ex/$NAME-merc.tar.bz2
+
+    ogr2ogr -t_srs EPSG:4326 ex/wgs84/$NAME.shp ex/merc/$NAME.shp
+    tar -C ex/wgs84 -cvf - ex/wgs84/$NAME.dbf ex/wgs84/$NAME.prj ex/wgs84/$NAME.shp ex/wgs84/$NAME.shx | bzip2 > ex/$NAME-latlon.tar.bz2
+done
+
+python <<SEND
 
 from os import stat
 from glob import glob
 from sys import stderr
 from os.path import basename
-from urllib import urlopen
 
-from boto.ec2 import EC2Connection
 from boto.s3.connection import S3Connection
 from boto.s3.bucket import Bucket
 
@@ -39,7 +73,7 @@ types = dict(bz2='application/x-bzip2', pbf='application/octet-stream')
 bucket = Bucket(S3Connection('$KEY', '$SECRET'), '$BUCKET')
 log = open('log.txt', 'a')
 
-for file in sorted(glob('ex/*.osm.???')):
+for file in sorted(glob('ex/*.osm.???')) + sorted(glob('ex/*.tar.bz2')):
     name = basename(file)
     type = types[name[-3:]]
     key = bucket.new_key(name)
@@ -52,7 +86,14 @@ log.close()
 key = bucket.new_key('log.txt')
 key.set_contents_from_file(open('log.txt'), policy='public-read', headers={'Content-Type': 'text/plain'})
 
+SEND
+
+python <<KILL
+
+from urllib import urlopen
+from boto.ec2 import EC2Connection
+
 instance = urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().strip()
 EC2Connection('$KEY', '$SECRET').terminate_instances(instance)
 
-CODE
+KILL
