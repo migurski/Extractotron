35560b1c1275fbad75482f4ea779db61daf4f1f4
Michal Migurski <mike@stamen.com>
2011-09-26 18:59:50 -0700

    Removed unneeded semicolon

--- run-instance.py
+++ run-instance.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         exit(1)
 
     user_data = open('extract.sh').read()
-    user_data = user_data.replace('osmosis.sh;\n', open('osmosis.sh').read())
+    user_data = user_data.replace('osmosis.sh\n', open('osmosis.sh').read())
     user_data = user_data.replace('$KEY', aws_key)
     user_data = user_data.replace('$SECRET', aws_secret)
     user_data = user_data.replace('$BUCKET', s3_bucket)
