57af4b5845a377365a44da4c82ea429d59584308
Michal Migurski <mike@stamen.com>
2011-12-11 13:40:03 -0800

    Improved comments

--- coastline-errors.sh
+++ coastline-errors.sh
@@ -1,15 +1,22 @@
 #!/bin/sh -x
 
+# 
+# This script expects to be run as the postgres user. It looks at the coastline
+# error checker's output file, coast/processed_p.shp, and pushes it through
+# PostGIS to find topology errors that might lead to rendering problems.
+# Results are saved to /tmp/coastline-errors.json and /tmp/coastline-missing.json.
+# 
+
 createdb coast > postgis.txt 2>&1
 createlang plpgsql coast >> postgis.txt 2>&1
 psql -f /usr/share/postgresql/8.4/contrib/postgis-1.5/postgis.sql coast >> postgis.txt 2>&1
 psql -f /usr/share/postgresql/8.4/contrib/postgis-1.5/spatial_ref_sys.sql coast >> postgis.txt 2>&1
 
 shp2pgsql -dID -s 900913 coast/processed_p.shp coast | psql coast >> postgis.txt 2>&1
+psql -c "DELETE FROM coast WHERE ST_IsValid(the_geom)" coast >> postgis.txt 2>&1
 
-psql -c "DELETE FROM coast WHERE ST_IsValid(the_geom)" coast
-psql -c "SELECT gid FROM coast ORDER BY gid" -tA -F ' ' coast > /tmp/gids
-psql -c "SELECT gid, tile_x, tile_y FROM coast ORDER BY gid" -tA -F ' ' coast > /tmp/tiles
+psql -tA -F ' ' -c "SELECT gid FROM coast ORDER BY gid" coast > /tmp/gids
+psql -tA -F ' ' -c "SELECT gid, tile_x, tile_y FROM coast ORDER BY gid" coast > /tmp/tiles
 
 rm -f /tmp/reasons
 
@@ -17,6 +24,9 @@ for GID in `cat /tmp/gids`; do
     psql -tA -F ' ' -c "SELECT gid, ST_IsValidReason(the_geom) FROM coast WHERE gid=$GID" coast >> /tmp/reasons
 done
 
+#
+# Creation of GeoJSON files is done in Python using the files found above.
+#
 
 python <<COMPILE
 
