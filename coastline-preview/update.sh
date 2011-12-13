9b545bebe73b760ef56d4efda092b1bdcbdd7d56
Michal Migurski <migurski@tilefarm.stamen.com>
2011-12-12 22:21:38 -0800

    Added new post_errors and post_missing tables

--- coastline-preview/update.sh
+++ coastline-preview/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-for TABLE in processed_p processed_i coastline_p; do
+for TABLE in processed_p processed_i coastline_p post_errors post_missing; do
 
     curl -sL http://osm-metro-extracts.s3.amazonaws.com/$TABLE-merc.tar.bz2 | bzcat | tar -xf -
     shp2pgsql -dID -s 900913 $TABLE.shp $TABLE | psql -U osm planet_osm
