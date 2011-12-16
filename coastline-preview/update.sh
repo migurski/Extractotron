eb70c9f183fbb93651cf94c9c7bafea0384e9abb
Michal Migurski <migurski@tilefarm.stamen.com>
2011-12-15 22:54:01 -0800

    Switched to correct local dbname

--- coastline-preview/update.sh
+++ coastline-preview/update.sh
@@ -3,11 +3,11 @@
 for TABLE in processed_p processed_i coastline_p post_errors post_missing; do
 
     curl -sL http://osm-metro-extracts.s3.amazonaws.com/$TABLE-merc.tar.bz2 | bzcat | tar -xf -
-    shp2pgsql -dID -s 900913 $TABLE.shp $TABLE | psql -U osm planet_osm
+    shp2pgsql -dID -s 900913 $TABLE.shp $TABLE | psql -U osm coastline
     rm $TABLE.???
     
-    psql -c "DELETE FROM $TABLE WHERE NOT ST_IsValid(the_geom)" -U osm planet_osm
+    psql -c "DELETE FROM $TABLE WHERE NOT ST_IsValid(the_geom)" -U osm coastline
 
 done
 
-# psql -U osm planet_osm < update.pgsql
+# psql -U osm coastline < update.pgsql
