b77ae1a82baa9c5ab0f61692b193dacf07fa86ef
Marc Puig <mpuig@qibbus.com>
2013-02-14 08:20:15 +0100

    Fixing planet-latest.osm.bz2 URL

--- extract.sh
+++ extract.sh
@@ -54,23 +54,23 @@ chmod +x coastline-errors.sh
 curl -sOL http://dev.openstreetmap.org/~bretth/osmosis-build/osmosis-latest.tgz
 tar -xzf osmosis-latest.tgz
 
-curl -OL "http://planet.openstreetmap.org/planet-latest.osm.bz2" > download.txt 2>&1
+curl -OL "http://planet.openstreetmap.org/planet/planet-latest.osm.bz2" > download.txt 2>&1
 
 echo '# extract', `date` >> log.txt
 mkdir ex
 
 (
     mkdir coast
-    
+
     cc/osm2coast planet-latest.osm.bz2 | gzip > coast/coastline.osm.gz
     cc/merge-coastlines.pl coast/coastline.osm.gz > coast/coast-merged.txt
     cc/coast2shp coast/coast-merged.txt coast/coastline.osm.gz coast/coastline > /dev/null
     cc/closeshp coast/coastline_c coast/coastline_i coast/processed > /dev/null
     shapeindex coast/coastline_c coast/coastline_i coast/coastline_p coast/processed_p coast/processed_i
-    
+
     # this creates /tmp/coastline-errors.json and /tmp/coastline-missing.json
     sudo -u postgres ./coastline-errors.sh > coastline-errors.txt 2>&1
-    
+
     ogr2ogr coast/post_errors.shp /tmp/coastline-errors.json
     ogr2ogr coast/post_missing.shp /tmp/coastline-missing.json
 ) &
