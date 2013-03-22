7a441ac6a3b4fe676dd89159398f4a1de54d65a3
Michal Migurski <mike-git@teczno.com>
2013-03-22 01:10:59 -0500

    Updated osmosis.sh with continent groups

--- build-osmosis-script.py
+++ build-osmosis-script.py
@@ -1,7 +1,9 @@
 from sys import argv, stderr
+from itertools import groupby
 from csv import DictReader
 
-cities = list(DictReader(open('cities.txt'), dialect='excel-tab'))
+group = lambda city: city['group']
+cities = sorted(DictReader(open('cities.txt'), dialect='excel-tab'), key=group)
 
 try:
     (osmosis, ) = argv[1:]
@@ -11,20 +13,35 @@ except ValueError:
 
 osmosis = open(osmosis, 'w')
 
-print >> osmosis, 'bunzip2 -c planet-latest.osm.bz2 | osmosis-*/bin/osmosis --rx file=- \\'
-print >> osmosis, '    --log-progress interval=60 \\'
-#print >> osmosis, '    --tee outputCount=2 \\'
-#print >> osmosis, '    --tag-filter accept-ways natural=coastline --used-node \\'
-#print >> osmosis, '    --wx coastline.osm.bz2 \\'
-print >> osmosis, '    --tee outputCount=%d \\' % len(cities)
-print >> osmosis, '    \\'
+lines = []
+groups = 0
 
-for city in cities:
-    xmin, xmax = min(float(city['left']), float(city['right'])), max(float(city['left']), float(city['right']))
-    ymin, ymax = min(float(city['bottom']), float(city['top'])), max(float(city['bottom']), float(city['top']))
-    print >> osmosis, '    --bb top=%(ymax).3f left=%(xmin).3f bottom=%(ymin).3f right=%(xmax).3f \\' % locals()
-    print >> osmosis, '        --tee outputCount=2 --wx file=ex/%(slug)s.osm.bz2 --wb file=ex/%(slug)s.osm.pbf \\' % city
+for (group, cities) in groupby(cities, group):
+    cities = list(cities)
+    
+    xs = [float(city['left']) for city in cities] + [float(city['right']) for city in cities]
+    ys = [float(city['bottom']) for city in cities] + [float(city['top']) for city in cities]
+    
+    xmin, xmax = min(xs), max(xs)
+    ymin, ymax = min(ys), max(ys)
+    assert (xmin < xmax and ymin < ymax)
+    
+    lines.append('  --bb top=%(ymax).3f left=%(xmin).3f bottom=%(ymin).3f right=%(xmax).3f' % locals())
+    lines.append('  --b --tee outputCount=%d' % len(cities))
+    
+    for city in cities:
+        xmin, xmax = min(float(city['left']), float(city['right'])), max(float(city['left']), float(city['right']))
+        ymin, ymax = min(float(city['bottom']), float(city['top'])), max(float(city['bottom']), float(city['top']))
+        assert (xmin < xmax and ymin < ymax)
 
-print >> osmosis, ';'
+        lines.append('    --bb top=%(ymax).3f left=%(xmin).3f bottom=%(ymin).3f right=%(xmax).3f' % locals())
+        lines.append('    --tee outputCount=2 --wx file=ex/%(slug)s.osm.bz2 --wb file=ex/%(slug)s.osm.pbf' % city)
+    
+    groups += 1
 
+lines.insert(0, 'bzcat planet-latest.osm.bz2 | bin/osmosis --rx file=-')
+lines.insert(1, '--log-progress interval=60')
+lines.insert(2, '--tee outputCount=%d' % groups)
+
+osmosis.write(' \\\n'.join(lines) + ';\n')
 osmosis.close()
