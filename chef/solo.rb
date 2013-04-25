00fa66fc4770ab594e052ddc49972d235bde0aae
Michal Migurski <mike@teczno.com>
2013-04-24 20:40:03 -0700

    Moved chef solo config to solo.rb to match opscode documentation

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,3 +1,6 @@
+#
+# http://docs.opscode.com/config_rb_solo.html
+#
 file_cache_path  "/tmp/chef-solo"
 cookbook_path    "/usr/local/extractotron/chef"
 log_level        :info
--- run-install.sh
+++ run-install.sh
@@ -9,5 +9,5 @@ if [ ! `which chef-solo` ]; then
     gem install chef ohai --no-rdoc --no-ri
 fi
 
-chef-solo -c $EXTRACTO_HOME/chef/chefsoloconfig.rb \
+chef-solo -c $EXTRACTO_HOME/chef/solo.rb \
           -j $EXTRACTO_HOME/chef/role-ec2.json
