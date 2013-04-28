19c63e13241c37bed05c4287951768d30db802dd
Michal Migurski <mike@teczno.com>
2013-04-27 20:43:17 -0700

    Sending "extract" message from status.php to run-queue.py

--- run-queue.py
+++ run-queue.py
@@ -1,5 +1,52 @@
 #!/usr/bin/env python
-from datetime import datetime
+from os.path import join, dirname
+from subprocess import Popen
+from time import sleep, time
+from json import load
 
-with open('/tmp/ran-queue', 'w') as out:
-    print >> out, datetime.now()
+from pika import BlockingConnection, ConnectionParameters
+
+def setup_channel():
+    '''
+    '''
+    conn = BlockingConnection(ConnectionParameters('localhost'))
+    chan = conn.channel()
+    
+    chan.exchange_declare(exchange='exchangotron', durable=True, type='fanout')
+    chan.queue_declare(queue='testing-py', durable=True)
+    
+    return chan
+
+if __name__ == '__main__':
+
+    due = time() + 55
+    
+    role_path = join(dirname(__file__), 'chef/role-ec2.json')
+    
+    with open(role_path) as role_file:
+        role = load(role_file)
+        planet = role['planet']
+        workdir = role['workdir']
+    
+    print role
+    print planet
+    print workdir
+    
+    channel = setup_channel()
+    
+    while time() < due:
+        method, properties, body = channel.basic_get(queue='testing-py')
+        
+        print method
+        print properties
+        print body
+        
+        if body == 'extract':
+            extract = join(dirname(__file__), 'run-extract.py')
+            extract = Popen([extract, planet, join(workdir, 'history')])
+            extract.wait()
+        
+        if hasattr(method, 'delivery_tag'):
+            channel.basic_ack(delivery_tag=method.delivery_tag)
+
+        sleep(1)
--- www/status.php
+++ www/status.php
@@ -1,1 +1,44 @@
-<? phpinfo(); ?>
+<?php
+
+    if($_POST['action'] == 'go for it')
+    {
+        $conn = new AMQPConnection();
+        $conn->connect();
+        
+        $chan = new AMQPChannel($conn);
+        $exch = new AMQPExchange($chan);
+        $queu = new AMQPQueue($chan);
+
+        $exch->setName('exchangotron');
+        $exch->setFlags(AMQP_DURABLE);
+        $exch->setType('fanout');
+        $exch->declare();
+        
+        $queu->setName('testing-py');
+        $queu->setFlags(AMQP_DURABLE);
+        $queu->bind('exchangotron', '');
+        $queu->declare();
+        
+        $msg = $exch->publish('extract', '');
+        
+        if(!$msg)
+        {
+            echo "FFFUUUU\n";
+        }
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Untitled</title>
+</head>
+<body>
+
+    <form action="<?=$_SERVER['SCRIPT_NAME'] ?>" method="post">
+        <input name="action" type="submit" value="go for it" />
+    </form>
+
+</body>
+</html>
