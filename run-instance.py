8d9ff23593bfd4eb6ae71367417537995827095d
Michal Migurski <mike-git@teczno.com>
2013-05-03 20:45:52 -0500

    Updated run-instance script with correct user-data

--- run-instance.py
+++ run-instance.py
@@ -1,14 +1,12 @@
+#!/usr/bin/env python
 """ Run an extractor instance.
 """
 from optparse import OptionParser
 from boto.ec2 import EC2Connection
-from httplib import HTTPConnection
-from urlparse import urljoin
-from urllib import urlencode
 
 parser = OptionParser(usage="%prog [options] <s3 bucket>")
 
-defaults = dict(ami_id='ami-bffa6fd6', type='m2.4xlarge', run=True, upload=True, kill=True)
+defaults = dict(ami_id='ami-096d0060', type='m2.4xlarge', run=True, upload=True, kill=True)
 
 parser.set_defaults(**defaults)
 
@@ -27,56 +25,39 @@ parser.add_option('--no-upload', dest='upload', action='store_false',
 parser.add_option('--no-kill', dest='kill', action='store_false',
                   help="Don't instruct the instance to kill itself at the end.")
 
-def post_script(filename):
-    """
-    """
-    form = dict(language='Bash', title=filename)
-    form.update(dict(content=open(filename).read()))
-
-    body = urlencode(form)
-    head = {'Content-Length': len(body), 'Content-Type': 'application/x-www-form-urlencoded'}
-    conn = HTTPConnection('dpaste.com')
-
-    conn.request('POST', '/api/v1/', headers=head)
-    conn.send(body)
-    
-    resp = conn.getresponse()
-    
-    if resp.status not in range(300, 399):
-        print resp.getheaders()
-        raise Exception('Received status %d from dpaste.com' % resp.status)
-    
-    href = resp.getheader('location')
-    href = urljoin(href, 'plain/')
-    
-    return href
-
 if __name__ == '__main__':
     
-    try:
-        options, (s3_bucket, ) = parser.parse_args()
-    except ValueError:
-        parser.print_usage()
-        exit(1)
+    options, args = parser.parse_args()
     
-    user_data = open('extract.sh').read()
+    user_data = open('bootstrap.sh').read()
     ec2 = EC2Connection()
     
+    if options.upload or options.kill:
+        user_data += '''
+#
+# Leave AWS credentials someplace useful.
+#
+echo '[Credentials]' > /etc/boto.cfg
+echo 'aws_access_key_id = %s' >> /etc/boto.cfg
+echo 'aws_secret_access_key = %s' >> /etc/boto.cfg
+''' % (ec2.access_key, ec2.secret_key)
+
     if options.upload:
-        user_data += open('upload-files.sh').read()
+        user_data += '''
+#
+# Upload completed files.
+#
+/usr/local/extractotron/run-upload.py
+'''
 
     if options.kill:
-        user_data += open('kill-self.sh').read()
+        user_data += '''
+#
+# Kill instance.
+#
+/usr/local/extractotron/run-terminate.py
+'''
 
-    user_data = user_data.replace('$KEY', ec2.access_key)
-    user_data = user_data.replace('$SECRET', ec2.secret_key)
-    user_data = user_data.replace('$BUCKET', s3_bucket)
-    user_data = user_data.replace('$OSMOSIS_HREF', post_script('osmosis.sh'))
-    user_data = user_data.replace('$OSM2PGSQL_HREF', post_script('osm2pgsql.sh'))
-    user_data = user_data.replace('$COASTSHAPES_HREF', post_script('coastshapes.sh'))
-    user_data = user_data.replace('$COASTERRORS_HREF', post_script('coastline-errors.sh'))
-    user_data = user_data.replace('$OSM2STYLE_HREF', post_script('osm2pgsql.style'))
-    
     if options.run:
         # bid a penny over median
         history = ec2.get_spot_price_history(instance_type=options.type)
